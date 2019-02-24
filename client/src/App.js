7e29990374fade9ec4d4ee0b101c8ec01d88c628
Ross Anderson <rosscoryanderson@gmail.com>
2019-02-24 19:25:12 +0800

    Added the initial styling for the dashboard, nav and project item components

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Dashboard from './components/Dashboard';
 import Navbar from './components/Layout/Navbar';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render() {
